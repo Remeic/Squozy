7bca73fef2c624c79adb3e67e79a78e39b8e6a2a
Remeic <fagioli.giulio@gmail.com>
2017-02-02 18:52:08 +0100

    Fixed bug on api post request, url validation is now working correctly

--- redirector/views.py
+++ redirector/views.py
@@ -50,15 +50,21 @@ def api_url_response(request,code):
 @csrf_exempt
 def api_url_request(request):
     if request.method=="POST":
-        post_url=request.POST['url']
-        url_validator = URLValidator()
-        try:
-            url_validator=(post_url)
-            result=UrlShrinked(url=post_url)
-            result.save()
-            result.publish()
-        except Exception as e:
-            result(url="null",shrinked_code="null")
+        url_verification=URLValidator()
+        post_url=request.POST.get('url');
+        result=UrlShrinked(url="")
+        if post_url:
+            try:
+                url_verification(post_url)
+                result.url=post_url
+                result.save()
+                result.publish()
+            except Exception as e:
+                result.url="url_invalid"
+                result.shrinked_code="url_invalid"
+        else:
+            result.url="url_empty"
+            result.shrinked_code="url_empty"
     object_response={'url':result.url,'shrink':result.shrinked_code}
     return HttpResponse(JsonResponse(object_response), content_type="application/json")
 
