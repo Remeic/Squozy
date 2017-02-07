fe7f494120b33c43b20b6e6402aa298c555520b8
Remeic <fagioli.giulio@gmail.com>
2017-02-07 19:10:50 +0100

    Test Env Heroku

--- djsite/settings.py
+++ djsite/settings.py
@@ -76,12 +76,26 @@ WSGI_APPLICATION = 'djsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+NAME_DB= os.getenv('NAME_DB', 'Ringo')#take name from heroku dash
+USERNAME_DB= os.getenv('USERNAME_DB', 'George')#take name from heroku dash
+PASSWD_DB= os.getenv('PASSWD_DB', 'Paul')#take name from heroku dash
+HOST_DB= os.getenv('HOST_DB', 'John')#take name from heroku dash
+SALT_KEY = os.getenv('SALT_KEY', 'your_salt_key')
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': NAME_DB,
+        'USER': USERNAME_DB,
+        'PASSWORD': PASSWD_DB,
+        'HOST': HOST_DB,
+        'PORT': '5432',
     }
 }
+
+DEBUG = True
 
 
 # Password validation
@@ -134,10 +148,6 @@ ALLOWED_HOSTS = ['*']
 
 DEBUG = False
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
 
 try:
     from .private_settings import *
