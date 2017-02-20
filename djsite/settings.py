dbc788514663151e34a2096c70e0cd257bd69a08
sᴀʟᴠᴀᴛᴏʀᴇ <svt.busiello@gmail.com>
2017-02-20 22:55:22 +0100

    Optimized Static Root and Open Graph Data

--- djsite/settings.py
+++ djsite/settings.py
@@ -113,7 +113,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/redirector/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
