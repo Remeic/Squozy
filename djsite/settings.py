8859d340e2a94405d5548836ffc4e52b63109fa2
sᴀʟᴠᴀᴛᴏʀᴇ <svt.busiello@gmail.com>
2017-10-03 15:33:49 +0200

    Updated repository - clean of useless requirements - module of heroku settings - updated git ignore

--- djsite/settings.py
+++ djsite/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -24,13 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8o6s119y7_nawwdk0mdm6u4)938q28chg2f6&eo45x3&$k#k8d'
 
 # This must be 32 keys base64 encoded
-FIELD_ENCRYPTION_KEY = os.environ.get('FIELD_ENCRYPTION_KEY', 'WUo5V1FxdGJNM0FUYjdoNDNQbndjM0hNMzlaQWRUeFo=')
+FIELD_ENCRYPTION_KEY = os.environ.get(
+    'FIELD_ENCRYPTION_KEY', 'WUo5V1FxdGJNM0FUYjdoNDNQbndjM0hNMzlaQWRUeFo=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -43,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'redirector',
     'encrypted_model_fields',
-
 ]
 
 MIDDLEWARE = [
@@ -76,49 +74,34 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djsite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-NAME_DB= os.getenv('NAME_DB', 'Ringo')#take name from heroku dash
-USERNAME_DB= os.getenv('USERNAME_DB', 'George')#take name from heroku dash
-PASSWD_DB= os.getenv('PASSWD_DB', 'Paul')#take name from heroku dash
-HOST_DB= os.getenv('HOST_DB', 'John')#take name from heroku dash
-SALT_KEY = os.getenv('SALT_KEY', 'your_salt_key')
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': NAME_DB,
-        'USER': USERNAME_DB,
-        'PASSWORD': PASSWD_DB,
-        'HOST': HOST_DB,
-        'PORT': '5432',
-    }
-}
 
 DEBUG = False
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
@@ -133,26 +116,38 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-
 import dj_database_url
 if os.getcwd() == "/app":
-    DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
 DEBUG = False
-
+HEROKU_MODE = True
 
 try:
     from .local_settings import *
 except ImportError:
-    print ("\n\nWARNING: No local_settings.py found! Please look at the README.md file!\n\n")
+    print(
+        "\n\nWARNING: No local_settings.py found! Please look at the README.md file!\n\n"
+    )
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+if HEROKU_MODE:
+    from .heroku_settings import *
